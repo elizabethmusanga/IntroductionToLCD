4a3255b70f56ac2a4937c5db9a6927c039b8fa23
elizabeth muanga <elizabeth.n.musanga@gmail.com>
2021-10-27 06:04:41 +0300

    edited display message

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 /*Our main program*/ 
 int main(void) { 
 	LCD_Init(); /* Initialize LCD */ 
-	LCD_String("Lesson1: Hello"); /* Write a string on 1st line of LCD*/ 
+	LCD_String("MSP II: Hello"); /* Write a string on 1st line of LCD*/ 
 	LCD_Cmd(0xC0); /* Go to 2nd line*/ 
 	LCD_String("Elizabeth"); /* Write string on 2nd line*/
 }
